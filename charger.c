9896dd8c54cf2fc986f53bbf76ee30b22b0b120d
WajdiG <Wajdi.Guedouar.Etu@univ-lemans.fr>
2014-11-19 09:34:31 +0100

    add .gitignore charger.c

---
+++ charger.c
@@ -0,0 +1,15 @@
+/**
+*\file charger.c
+*\brief fonction permttant de charger les paramètres du jeu, les caractéristique du joueur et l'avancement dans le jeu
+*\author Wajdi Guedouar
+*\version 0.1
+*\date 19 Novembre 2014
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "crea_avatar.h"
+extern t_joueur joueur;
+extern FILE*fichier;
+
