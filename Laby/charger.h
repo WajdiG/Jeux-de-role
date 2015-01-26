32b4d65ec04e90d714e66099df6b21418e509922
WajdiG <w.guedouar@laposte.net>
2015-01-26 12:38:30 +0100

    Update charger.h

--- Laby/charger.h
+++ Laby/charger.h
@@ -14,7 +14,7 @@
 
 /**
 *\fn charger_jeu(t_joueur*joueur,t_coord*coord, char**monde [X][Y],FILE*fichier)
-*\brief fonction permettant de charger toutes les données contenue dans le fichier de sauvegarde dans la structure t_joueur jouer
+*\brief fonction permettant de charger toutes les données contenues dans le fichier de sauvegarde dans la structure t_joueur joueur
 *\param *joueur pointeur sur la structure t_joueur joueur
 *\param coord coordonnées du joueur dans la matrice monde
 *\param monde[X][Y] matrice monde représentant le monde dans lequel le joueur évolue
@@ -29,7 +29,7 @@ void charger_jeu(t_joueur *joueur,t_coord *coord,char ** monde[X][Y],FILE*fichie
 *\param joueur pointeur sur la structure t_joueur joueur
 *\param coord coordonnées du joueur dans la matrice monde
 *\param monde[X][Y] matrice monde représentant le monde dans lequel le joueur évolue
-*\return int renvoie 1 si le fichier charger est valide, 0 si non
+*\return int renvoie 1 si le fichier chargé est valide, 0 si non
 */
 int charger_partie(char* charger,t_joueur* joueur,t_coord *coord, char ** monde[X][Y]);
 
